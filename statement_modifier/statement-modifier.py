62c3e907a91b4a3090195b3611e4b53f26524713
mac <logianwilliam@gmail.com>
2025-01-03 18:58:10 -0500

    working test of modify_statements

--- statement_modifier/statement-modifier.py
+++ statement_modifier/statement-modifier.py
@@ -1,14 +1,24 @@
 import json
 
+with open("statement_modifier/base-claims.json", "r") as file:
+    bclaims = json.load(file)
+with open("statement_modifier/modifiers.json", "r") as file:
+    modifiers = json.load(file)
+
 def modify_statements(base_claims, modifiers):
     modified_statements = []
     for statement in base_claims:
         for modifier in modifiers:
             prompt = modifier["modifier"].replace("{statement}", statement["statement"])
-            truth = 1 - statement[truth] if modifier["negation"] else statement["truth"]
+            truth = 1 # - statement[truth] if modifier["negation"] else statement["true"]
             modified_statements.append({
                 "prompt": prompt,
                 "modifier_id": modifier["id"],
-                "truth": truth,
-                "statement_id": statement["id"]
-            })
+                "true": truth,
+                "statement_id": statement["id"],
+                "topic": statement["topic"],
+                "indicator_category": modifier["category"]
+                })
+    return modified_statements
+
+print(modify_statements(bclaims, modifiers))
