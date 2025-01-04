85e8350e2f824355a4ed510057e2dd26b37b9614
mac <logianwilliam@gmail.com>
2025-01-04 16:34:37 -0500

    statement modifier finished with minor issues

--- statement_modifier/statement-modifier.py
+++ statement_modifier/statement-modifier.py
@@ -1,3 +1,5 @@
+#   notes: some characters from doc give weird formatting issues, resolve capitalization too (proper nouns make this hard)
+
 import json
 
 with open("statement_modifier/base-claims.json", "r") as file:
@@ -8,9 +10,17 @@ with open("statement_modifier/modifiers.json", "r") as file:
 def modify_statements(base_claims, modifiers):
     modified_statements = []
     for statement in base_claims:
+        modified_statements.append({
+                "prompt": statement["statement"],
+                "modifier_id": "",      # TODO: Determine what we do for unmodified data
+                "true": statement["true"],
+                "statement_id": statement["id"],
+                "topic": statement["topic"],
+                "indicator_category": ""
+                })
         for modifier in modifiers:
             prompt = modifier["modifier"].replace("{statement}", statement["statement"])
-            truth = 1 # - statement[truth] if modifier["negation"] else statement["true"]
+            truth = 1 - statement["true"] if modifier["negation"] else statement["true"]
             modified_statements.append({
                 "prompt": prompt,
                 "modifier_id": modifier["id"],
@@ -21,4 +31,5 @@ def modify_statements(base_claims, modifiers):
                 })
     return modified_statements
 
-print(modify_statements(bclaims, modifiers))
+with open("statement_modifier/modified-claims.json", "w") as file:
+    json.dump(modify_statements(bclaims, modifiers), file, indent=4)
